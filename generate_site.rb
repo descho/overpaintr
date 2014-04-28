56218c9851e1686a1850b4e3861aacd6cfbbb8ab
Des Cho <d.chongarova@gmail.com>
2014-04-28 23:36:59 +0300

    Template already works and generates both languages.

--- generate_site.rb
+++ generate_site.rb
@@ -1,4 +1,5 @@
-File.write("./en/index.html",'
+
+template = '
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -42,18 +43,22 @@ File.write("./en/index.html",'
 			</div>
 		</section>
 	</body>
-</html>')
+</html>'
 
 en_text = {
 	:tagline => "An app that lets you report racist graffiti.",
 	:email_input_placeholder => "Enter email",
 	:call_to_action => "Subscribe for updates",
-	:language => "<a href='../bg/index.html'>Български</a>",
+	:language => "<a href='../bg/index.html'>Бг</a>",
 }
 
 bg_text = {
 	:tagline => "Приложение, с което можеш да докладваш расистки графити.",
 	:email_input_placeholder => "Въведи имейл",
 	:call_to_action => "Запиши се за новини",
-	:language => "<a href='../en/index.html'>English</a>",
+	:language => "<a href='../en/index.html'>En</a>",
 }
+
+File.write("./en/index.html", template%en_text)
+File.write("./bg/index.html", template%bg_text)
+
